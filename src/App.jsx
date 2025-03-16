18c81ccc1979daff9ae5de4ff7dc7d0c7da088dd
sujansijakhow <sujansijakhow@gmail.com>
2025-03-16 21:09:42 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+   <div className='w-full flex h-screen justify-center items-center p-4'>
+      <div className='border-[1px] border-gray-700 max-w-6xl w-full min-h-[600px] rounded-lg'>
+        {/* header */}
+        <div>
+          header
+        </div>
+      </div>
+   </div>
+  )
+}
+
+export default App
